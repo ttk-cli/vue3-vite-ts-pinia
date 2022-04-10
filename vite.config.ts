96520c797c1816d378e114862af2bdcafae39432
allen <272970666@qq.com>
2022-04-10 19:12:00 +0800

    feat: 路径别名&自动导入

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 import vue from '@vitejs/plugin-vue'
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
+  plugins: [
+    AutoImport({
+      dts: 'src/auto-imports.d.ts', // 可以自定义文件生成的位置，默认是根目录下
+      imports: ['vue'],
+    }),
+    Components({
+      // 指定组件位置，默认是src/components
+      dirs: ['src/components'],
+      // ui库解析器
+      // resolvers: [ElementPlusResolver()],
+      extensions: ['vue'],
+      // 配置文件生成位置
+      dts: 'src/components.d.ts',
+    }),
+    vue(),
+  ],
+  server: {
+    port: 3000,
+    open: true, //自动打开
+    base: './ ', //生产环境路径
+  },
 })
