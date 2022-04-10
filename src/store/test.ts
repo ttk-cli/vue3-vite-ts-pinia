00a25a9a5c259d382934d7aaa045d65242964b1b
allen <272970666@qq.com>
2022-04-11 00:59:05 +0800

    feat: add pinia

---
+++ src/store/test.ts
@@ -0,0 +1,27 @@
+//src/store/test.ts
+
+import { defineStore } from 'pinia'
+
+export const useTestStore = defineStore({
+  id: 'test',
+  persist: {
+    // 开启持久化
+    enabled: true,
+  },
+  state: () => {
+    return {
+      name: '张三',
+      token: 'token...',
+    }
+  },
+  getters: {
+    fullName: ({ name }) => {
+      return name + '丰'
+    },
+  },
+  actions: {
+    updateName(name: string) {
+      this.name = name
+    },
+  },
+})
--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -2,12 +2,25 @@
   <div>控制台</div>
   <div @click="pushRouter('test1_1')">test1_1</div>
   <div @click="pushRouter('test2')">test2</div>
+  <div>name:{{ name }}</div>
+  <div>fullName:{{ fullName }}</div>
+  <div @click="updateName">updateName</div>
 </template>
 
 <script lang="ts" setup name="dashboard">
+import { useTestStore } from '@/store/test'
+import { storeToRefs } from 'pinia'
+
 const router = useRouter()
 function pushRouter(path: string) {
   router.push(path)
+}
+
+const testStore = useTestStore()
+const { name, fullName } = storeToRefs(testStore)
+
+function updateName() {
+  testStore.updateName('333')
 }
 </script>
 
