0a50651ed9bdb0707120adc7df27178ab086f62f
allen <272970666@qq.com>
2022-04-11 00:45:50 +0800

    feat: router demo

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,7 +1,14 @@
 <template>
   <div>控制台</div>
+  <div @click="pushRouter('test1_1')">test1_1</div>
+  <div @click="pushRouter('test2')">test2</div>
 </template>
 
-<script lang="ts" setup name="dashboard"></script>
+<script lang="ts" setup name="dashboard">
+const router = useRouter()
+function pushRouter(path: string) {
+  router.push(path)
+}
+</script>
 
 <style lang="scss" scoped></style>
