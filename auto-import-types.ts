bc4e0fb4e65fe743c4a9deae4ca11d7a46b147e2
allen <272970666@qq.com>
2022-05-09 02:27:45 +0800

    feat: update auto-import-types

--- auto-import-types.ts
+++ auto-import-types.ts
@@ -1,38 +1,45 @@
 import { promises as fs } from 'fs'
+import { resolve } from 'path'
 
 type Options = Partial<{
-  target: string
+  typesPath: string
   filepath: string
   globalsPropValue: boolean | 'readonly' | 'readable' | 'writable' | 'writeable'
 }>
 
 const defaultOptions = {
-  target: '@/@types',
+  typesPath: 'src/@types',
   filepath: '.eslintrc-auto-import-types.json',
   globalsPropValue: true,
 }
 
-export default function (options?: Options) {
-  // eslint-disable-next-line no-param-reassign
-  options = Object.assign(defaultOptions, options)
+export default function (options: Options = {}) {
+  Object.assign(options, defaultOptions)
+
+  const { typesPath, filepath, globalsPropValue } = options
+  const dirPath = resolve(__dirname, typesPath)
+
+  async function generateConfigFiles() {
+    const filesPath = await fs.readdir(dirPath)
+    const eslintConfigs = { globals: {} }
+    const namespaces = []
+    for (const filePath of filesPath) {
+      const file = await fs.readFile(dirPath + '\\' + filePath, 'utf-8')
+      namespaces.push(...file.match(/(?<=namespace )[\S]*/g))
+    }
+    for (const namespace of namespaces) {
+      eslintConfigs.globals[namespace] = globalsPropValue
+    }
+    fs.writeFile(filepath, JSON.stringify(eslintConfigs, null, 2), 'utf-8')
+  }
+
+  generateConfigFiles()
   return {
     name: 'auto-import-types',
-    async config(config) {
-      const { target, filepath, globalsPropValue } = options
-      // console.log(config.resolve.alias['@'])
-      // console.log(target.replace('@/', config.resolve.alias['@'] + '\\'))
-      const dirPath = target.replace('@/', config.resolve.alias['@'] + '\\')
-      const filesPath = await fs.readdir(dirPath)
-      const eslintConfigs = { globals: {} }
-      const namespaces = []
-      for (const filePath of filesPath) {
-        const file = await fs.readFile(dirPath + '\\' + filePath, 'utf-8')
-        namespaces.push(...file.match(/(?<=namespace )[a-zA-Z]*/g))
+    handleHotUpdate(ctx) {
+      if (ctx.file.includes(typesPath)) {
+        generateConfigFiles()
       }
-      for (const namespace of namespaces) {
-        eslintConfigs.globals[namespace] = globalsPropValue
-      }
-      fs.writeFile(filepath, JSON.stringify(eslintConfigs, null, 2), 'utf-8')
     },
   }
 }
