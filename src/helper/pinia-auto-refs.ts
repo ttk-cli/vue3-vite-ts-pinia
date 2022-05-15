aa97f99217e1eb3830efc209673792d4f6a8fcea
allen <272970666@qq.com>
2022-05-15 10:59:58 +0800

    chore: update pinia-auto-refs

--- src/helper/pinia-auto-refs.ts
+++ src/helper/pinia-auto-refs.ts
@@ -3,9 +3,9 @@ import appStore from '@/store/app'
 import testStore from '@/store/test'
 import userStore from '@/store/user'
 
-import { ToRef, StoreToRefs } from 'vue'
+import { ToRef, AutoToRefs } from 'vue'
 declare module 'vue' {
-  export type StoreToRefs<T> = {
+  export type AutoToRefs<T> = {
     [K in keyof T]: T[K] extends Function ? T[K] : ToRef<T[K]>
   }
 }
@@ -19,5 +19,5 @@ const storeExports = {
 export function useStore<T extends keyof typeof storeExports>(storeName: T) {
   const store = storeExports[storeName]()
   const storeRefs = storeToRefs(store)
-  return { ...store, ...storeRefs } as unknown as StoreToRefs<ReturnType<typeof storeExports[T]>>
+  return { ...store, ...storeRefs } as unknown as AutoToRefs<ReturnType<typeof storeExports[T]>>
 }
