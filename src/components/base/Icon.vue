10554ccc128e15c4c447634a7d76a7382def2151
allen <272970666@qq.com>
2022-05-20 13:00:21 +0800

    feat: update Icon props

--- src/components/base/Icon.vue
+++ src/components/base/Icon.vue
@@ -3,17 +3,22 @@
 </template>
 
 <script setup lang="ts">
-const props = defineProps({
-  icon: {
-    type: String,
-  },
-  w: {
-    type: String,
-  },
-  h: {
-    type: String,
-  },
-})
+// const props = defineProps({
+//   icon: {
+//     type: String,
+//   },
+//   w: {
+//     type: String,
+//   },
+//   h: {
+//     type: String,
+//   },
+// })
+const props = defineProps<{
+  icon: string
+  w?: string
+  h?: string
+}>()
 
 const className = computed(() => {
   const { icon, w, h } = props
