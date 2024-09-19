3c45a0879e643365b38a5cbf7bac546eab40469c
allen <guozenghui@doweidu.com>
2024-09-19 13:50:14 +0800

    feat: 样式优化

--- src/components/base/TreeMenu.vue
+++ src/components/base/TreeMenu.vue
@@ -8,13 +8,13 @@ defineProps<{
   <template v-for="menu in menus" :key="menu.path">
     <el-sub-menu v-if="menu.children && menu.children.length > 0" :index="menu.path">
       <template #title>
-        <div class="mr-10" :class="menu.meta.icon" />
+        <div class="mr-10 box-border min-w-24" :class="menu.meta.icon" />
         <span>{{ menu.meta.title }}</span>
       </template>
       <TreeMenu :menus="menu.children" />
     </el-sub-menu>
     <el-menu-item v-else :index="menu.path">
-      <div class="mr-10" :class="menu.meta.icon" />
+      <div class="mr-10 box-border min-w-24" :class="menu.meta.icon" />
       <template #title>
         {{ menu.meta.title }}
       </template>
